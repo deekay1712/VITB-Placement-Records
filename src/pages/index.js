97838ff51f6423f9b01fe5315275c844924d3d39
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2023-08-25 18:53:14 +0530

    :hammer: made css changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-const inter = Inter({ subsets: ['latin'] })
 import Row from '../components/Row'
 
 export default function Home({data}) {
@@ -13,9 +11,8 @@ export default function Home({data}) {
         <meta name="description" content="Developed by DeeKay" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" /> */}
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={styles.main}>
         {
           data.map((item, index) => {
             return <Row item={item} key={index} />
